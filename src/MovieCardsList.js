ecaf2e0f664896179f742768ea3f7a5c74513722
root <aarokan@gmail.com>
2019-09-19 12:14:59 +0000

    ES6 arrow function

--- src/MovieCardsList.js
+++ src/MovieCardsList.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import MovieCard from './MovieCard';
 
-function MovieCardsList (props) {
+
+
+const MovieCardsList = props => {
+    /*
+    Destructuring via ES6. We're getting the profiles, users, and movies properties
+    off of the pros passed into this presentational component. If you need a refresher on this syntax, check
+    out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
+    */
+  
     const { profiles, users, movies } = props;
   
     const usersByMovie = {};
